8a1a95c86f244ed2dd5b563f4626c3c95096efbd
Andrea Mangiatordi <andrea@Bambino.local>
2010-03-25 01:33:50 +0100

    Aggiunta di alcuni file che mi ero dimenticato e alcune piccole modifiche all POC.

--- farfalla.js
+++ farfalla.js
@@ -1,13 +1,5 @@
 $(function() {
 
-	$(window).scroll(function(){
-		if($('#monitor').height()<$(window).height()){
-			$('#monitor')
-			.css('margin-top', ($(window).scrollTop()) + 'px');
-//		.animate({'marginTop': ($(window).scrollTop() + 30) + 'px'}, 'fast' );
-		}
-	});
-	
 	$('<div id="monitor">')
 	.html('<p>This page is now farfallized</p>')
 	.addClass('monitor')
@@ -17,11 +9,19 @@ $(function() {
 		function(){ $(this).toggleClass('monitor_left'); }
 	);
 
-	$('<div id="highlighter">')
+	$(window).scroll(function(){
+		if($('#monitor').height()<$(window).height()){
+			$('#monitor')
+			.css('margin-top', ($(window).scrollTop()) + 'px');
+//		.animate({'marginTop': ($(window).scrollTop() + 30) + 'px'}, 'fast' );
+		}
+	});
+	
+/*	$('<div id="highlighter">')
 	.addClass('highlighter')
 	.appendTo('body')
 	.fadeIn(3000);
-	
+*/	
 /*	$('<div id="toolbar">')
 	.addClass('toolbar')
 	.prependTo('body')
@@ -48,6 +48,8 @@ $(function() {
 		$(this).hover(
 			function() {
 				$(this).addClass('farfalla_red');
+				$(this).parents().removeClass('farfalla_red');
+
 /*				$('#highlighter')
 					.width($(this).width()+'px')
 					.height($(this).height()+'px')
