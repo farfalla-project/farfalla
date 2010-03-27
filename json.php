959f5c654565c97cb0f555bc3ceff67dc0b6b8fe
Andrea Mangiatordi <andrea.mangiatordi@gmail.com>
2010-03-27 15:11:21 +0100

    Upload dei file di cakephp + alcuni file che simulano le chiamate json.
    Creazione delle directory dei singoli plugin.

---
+++ json.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+echo ('
+{
+	"keyboard" : {
+		"layout" : "qwerty"
+		},
+	"magnifier" : {
+		"ratio" : "2x"
+		}
+	
+}
+');
+
+?>
---
+++ plugins/keyboard/keyboard.farfalla.js
@@ -0,0 +1,10 @@
+// Virtual Keyboard
+
+$(function() {
+	
+	$('input[type=text], input[class=lst], input[type=password], textarea')
+		.keyboard({
+			layout:'qwerty'
+		});
+
+});
