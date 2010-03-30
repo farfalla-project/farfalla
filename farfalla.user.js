a83165f6c861573ff7fb8aa4510c1404a7ec70ac
Andrea Mangiatordi <andrea.mangiatordi@gmail.com>
2010-03-31 01:58:11 +0200

    Merged jQuery into farfalla.js, removed non necessary lines from the userscript, added jQuery-ui theme for virtual keyboard.

--- farfalla.user.js
+++ farfalla.user.js
@@ -2,30 +2,14 @@
 // @name           Farfalla
 // @namespace      userscripts.org
 // @description    Injects Farfalla helper
-// @version        0.1
+// @version        0.2
 // @include        *
 // ==/UserScript==
 	
 
 var headID = document.getElementsByTagName("head")[0];         
-var farfallaCss = document.createElement('link');
-var jqueryScript = document.createElement('script');
-var jqueryuiScript = document.createElement('script');
-var jokScript = document.createElement('script');
 var farfallaScript = document.createElement('script');
-farfallaCss.type = 'text/css';
-farfallaCss.rel = 'stylesheet';
-farfallaCss.href = 'http://localhost/farfalla/farfalla.css';
-jqueryScript.type = 'text/javascript';
-jqueryScript.src = 'http://localhost/farfalla/jquery-1.3.2.min.js';
-jqueryuiScript.type = 'text/javascript';
-jqueryuiScript.src = 'http://localhost/farfalla/jquery-ui-1.7.2.custom.min.js';
-jokScript.type = 'text/javascript';
-jokScript.src = 'http://localhost/farfalla/jquery.keyboard.js';
 farfallaScript.type = 'text/javascript';
 farfallaScript.src = 'http://localhost/farfalla/farfalla.js';
-headID.appendChild(farfallaCss);
-headID.appendChild(jqueryScript);
-headID.appendChild(jqueryuiScript);
-headID.appendChild(jokScript);
 headID.appendChild(farfallaScript);
+
