d42949c477d993f281971467187a83cf307d50e2
Andrea Mangiatordi <andrea.mangiatordi@gmail.com>
2011-01-30 21:32:08 +0100

    Text-to-speech plugin working.

--- plugins/text-to-speech/text-to-speech.farfalla.js
+++ plugins/text-to-speech/text-to-speech.farfalla.js
@@ -5,19 +5,26 @@ $(function() {
 
   $('<div id="farfalla_jplayer">').prependTo('body');
 
-  $('#farfalla_buttons').farfalla_add_button('say this','say_this',function(){
 
     $('#farfalla_jplayer').jPlayer({
-      ready: function () {
-        $(this).jPlayer("setMedia", {
-	      mp3: "http://speech.jtalkplugin.com/api/?speech="+$('#monitor').html()
-        }).jPlayer("play");
-      },
+//      ready: function () {
+        
+//     },
       swfPath: "./",
       supplied: "mp3"
     });
 //  alert($('#monitor').html());
-  return false;
+
+
+  $('#farfalla_buttons').farfalla_add_button('say this','say_this',function(){
+
+    $('#farfalla_jplayer').jPlayer("setMedia", {
+	   mp3: "http://speech.jtalkplugin.com/api/?speech="+$('#monitor').html()
+    }).jPlayer("play");
+
+    return false;
+
   });
+        
 
 });
