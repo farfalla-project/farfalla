5580b599d3217f1c89737f46b33de7295623d0d0
Andrea Mangiatordi <andrea.mangiatordi@gmail.com>
2012-02-06 10:28:43 +0100

    Google Analytics monitoring code added to farfalla.js

--- farfalla.js
+++ farfalla.js
@@ -101,3 +101,17 @@ $LAB
   head.js(farfalla_path+'libs/jquery.min.js', farfalla_path+'libs/jquery-ui.custom.min.js', farfalla_path+'libs/main.js', function() {
  
 });
+
+
+// Google analytics monitoring code
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-9777827-10']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
