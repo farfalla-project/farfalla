d62276c9a5adfdc9911d1f5654f6bcbc6f5b8101
Andrea Mangiatordi <andrea.mangiatordi@gmail.com>
2013-02-17 20:08:26 +0100

    Cakephp update in progress...

---
+++ backend/app/Config/Schema/schema.php
@@ -0,0 +1,41 @@
+<?php
+/* SVN FILE: $Id$ */
+/* App schema generated on: 2010-11-23 15:11:34 : 1290522094*/
+class AppSchema extends CakeSchema {
+	var $name = 'App';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+	var $plugins = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'length' => 100, 'key' => 'unique'),
+		'description' => array('type' => 'text', 'null' => false),
+		'created' => array('type' => 'datetime', 'null' => false),
+		'modified' => array('type' => 'datetime', 'null' => false),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'name' => array('column' => 'name', 'unique' => 1), 'name_2' => array('column' => 'name', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+	var $profiles = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'length' => 100, 'key' => 'unique'),
+		'password' => array('type' => 'string', 'null' => false, 'length' => 100),
+		'description' => array('type' => 'text', 'null' => false),
+		'created' => array('type' => 'datetime', 'null' => false),
+		'modified' => array('type' => 'datetime', 'null' => false),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'name' => array('column' => 'name', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+	var $profiles_plugins = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'profile_id' => array('type' => 'integer', 'null' => false, 'length' => 10),
+		'plugin_id' => array('type' => 'integer', 'null' => false, 'length' => 10),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+}
+?>
