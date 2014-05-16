add39a1012d53e454c9eaf74181edd0eec07de67
Andrea Mangiatordi <andrea.mangiatordi@gmail.com>
2014-05-16 13:37:10 +0200

    Added Google Analytics profiles to the six main plugins.

--- plugins/bigcursor/bigcursor.farfalla.js
+++ plugins/bigcursor/bigcursor.farfalla.js
@@ -1,6 +1,18 @@
 // Farfalla plugin: Big Cursor. Changes the default mouse cursor to an enlarged one with high contrast.
 // To do: adding options for distinct cursors.
 
+// Google analytics monitoring code
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-9777827-29', 'farfalla-project.org');
+  ga('send', 'pageview');
+
+
+
 jQuery.noConflict();
 (function($) {
 
--- plugins/keyboard/keyboard.farfalla.js
+++ plugins/keyboard/keyboard.farfalla.js
@@ -1,5 +1,19 @@
 // Farfalla plugin: Virtual Keyboard
 
+// Google analytics monitoring code
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-9777827-10']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
+
 jQuery.noConflict();
 (function($) { 
 
