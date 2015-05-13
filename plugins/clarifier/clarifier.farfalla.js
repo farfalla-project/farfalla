58ef2e58d3695edfd5d6c4f1a75983eb200fe9c7
Andrea Mangiatordi <andrea.mangiatordi@gmail.com>
2015-05-13 09:37:37 +0200

    Added uppercase function under the "clarifier" plugin, it still needs an icon...

--- plugins/clarifier/clarifier.farfalla.js
+++ plugins/clarifier/clarifier.farfalla.js
@@ -21,88 +21,128 @@
 // Farfalla plugin: Clarifier. Transforms the typography of a document in a Dyslexia-friendly fashion.
 
 // Google analytics monitoring code
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','_gafclarifier');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','_gafclarifier');
 
-  _gafclarifier('create', 'UA-9777827-28', {'cookieName':'_gafclarifier'});
-  _gafclarifier('send', 'pageview');
+_gafclarifier('create', 'UA-9777827-28', {'cookieName':'_gafclarifier'});
+_gafclarifier('send', 'pageview');
 
-/*
-      $f('html').each( function(){
-        console.log($f(this).css('background-color'));
-      })
-*/
-      $f.farfalla_add_css('clarifier','clarifier');
 
-      $f.clarifier_on = function () {
+$f.farfalla_create_plugin_options('clarifier');
 
-        $f('#clarifierActivator').farfalla_switch_on('clarifier');
+$f.farfalla_add_css('clarifier','clarifier');
 
-        $f('#farfalla_container *').addClass('donttouchme');
+// Adds or removes uppercase setting
 
-        // Set <html> background color to cream, but only in case it is white
-        if($f('html').css('background-color')=='transparent'||$f('html').css('background-color')=='rgba(0, 0, 0, 0)'){
-          $f('html').addClass('creamBackground');
-        }
+$f.clarifier_uppercase = function () {
+  $f.farfalla_get_option('uppercase', function(data){
+	if(data.value==1){
+      $f.farfalla_set_option('uppercase');
+      $f('.fontUppercase').removeClass('fontUppercase');
+    } else {
+      $f('*').addClass('fontUppercase');
+      $f.farfalla_set_option('uppercase',1);
+    }
+  })
+};
 
-        $f('*').not('.donttouchme').each(function(){
+// Adds an activation button for the global uppercase effect
 
-          if($f(this).css('background-color')=='rgb(255, 255, 255)'){
-            $f(this).addClass('creamBackground');
-          }
+$f.farfalla_add_ui('clarifier', 'button', 'clarifier_uppercase', 'uppercase', function(){
+  $f.clarifier_uppercase();
+});
 
-        });
+// Check if global uppercase setting is on and apply it if positive
 
-        $f('input, textarea').not('.donttouchme').addClass('inputClarifier');
+$f.clarifier_uppercase_init = function () {
+  $f.farfalla_get_option('uppercase', function(data){
+	if(data.value==1){
+      $f('*').addClass('fontUppercase');
+    }
+  })
+};
 
-        // Set fonts to Tahoma, Arial, sans
+$f.clarifier_on = function () {
+  $f('#clarifierActivator').farfalla_switch_on('clarifier');
+  $f('.plugin_options').not('#clarifier').slideUp('fast');
+  $f('#clarifier_options').slideDown('fast');
+  $f('#farfalla_container *').addClass('donttouchme');
 
-        $f('*').not('.donttouchme').addClass('fontClarifier');
+  // Set <html> background color to cream, but only in case it is white
+  if($f('html').css('background-color')=='transparent'||$f('html').css('background-color')=='rgba(0, 0, 0, 0)'){
+    $f('html').addClass('creamBackground');
+  }
+  $f('*').not('.donttouchme').each(function(){
+    if($f(this).css('background-color')=='rgb(255, 255, 255)'){
+      $f(this).addClass('creamBackground');
+    }
+  });
 
-        $f('h1').not('.donttouchme').addClass('h1Clarifier');
+  $f('input, textarea').not('.donttouchme').addClass('inputClarifier');
 
-        $f('h2').not('.donttouchme').addClass('h2Clarifier');
+  // Set fonts to Tahoma, Arial, sans
 
-        $f('h1, h2, h3, h4, h5, h6, h7, h8, h9, h10').not('.donttouchme').addClass('boldClarifier');
+  $f('*').not('.donttouchme').addClass('fontClarifier');
 
-        $f('*').not('.donttouchme').each(function(){
+  $f('h1').not('.donttouchme').addClass('h1Clarifier');
 
-          if($f(this).css('text-align')=='justify'){
-            $f(this).addClass('leftAlignClarifier');
-          }
+  $f('h2').not('.donttouchme').addClass('h2Clarifier');
 
-        });
+  $f('h1, h2, h3, h4, h5, h6, h7, h8, h9, h10').not('.donttouchme').addClass('boldClarifier');
 
-        // Set line height to 200% in <p> and <li> elements only
+  $f('*').not('.donttouchme').each(function(){
+    if($f(this).css('text-align')=='justify'){
+	  $f(this).addClass('leftAlignClarifier');
+    }
+  });
 
-        $f('p, li').not('.donttouchme').each(function(){
-          $f(this).addClass('linespacingClarifier');
-        });
+  // Set line height to 200% in <p> and <li> elements only
 
-      }
+  $f('p, li').not('.donttouchme').each(function(){
+    $f(this).addClass('linespacingClarifier');
+  });
 
-      $f.clarifier_off = function () {
+  $f.clarifier_uppercase_init();
 
-        $f('#clarifierActivator').farfalla_switch_off('clarifier');
-        $f('*').removeClass('creamBackground');
-        $f('input, textarea').removeClass('inputClarifier');
-        $f('*').removeClass('fontClarifier');
-        $f('h1').removeClass('h1Clarifier');
-        $f('h2').removeClass('h2Clarifier');
-        $f('h1, h2, h3, h4, h5, h6, h7, h8, h9, h10').removeClass('boldClarifier');
-        $f('*').removeClass('leftAlignClarifier');
-        $f('p, li').removeClass('linespacingClarifier');
+}
 
-      }
+$f.clarifier_off = function () {
+  $f('#clarifierActivator').farfalla_switch_off('clarifier');
+  $f('#clarifier_options').hide();
 
-    $f('#clarifier_options_switch').click( function(){
-      if($f(this).hasClass('plugin_options_switch_on')){
-        $f.clarifier_off()
-      } else {
-        $f.clarifier_on()
-      }
-    });
+  $f('#clarifierActivator').farfalla_switch_off('clarifier');
+  $f('*').removeClass('creamBackground');
+  $f('input, textarea').removeClass('inputClarifier');
+  $f('*').removeClass('fontClarifier');
+  $f('h1').removeClass('h1Clarifier');
+  $f('h2').removeClass('h2Clarifier');
+  $f('h1, h2, h3, h4, h5, h6, h7, h8, h9, h10').removeClass('boldClarifier');
+  $f('*').removeClass('leftAlignClarifier');
+  $f('p, li').removeClass('linespacingClarifier');
+  $f('.fontUppercase').removeClass('fontUppercase');
 
-    $f.clarifier_on();
+}
+
+$f.clarifier_on();
+
+$f('#clarifier_options_deactivate').click( function() {
+
+  $f.clarifier_off();
+
+});
+
+$f('#clarifierActivator').click( function(){
+
+  $f.clarifier_on()
+
+});
+
+$f('#clarifier_options_switch').click( function(){
+  if($f(this).hasClass('plugin_options_switch_on')){
+	$f.clarifier_off()
+  } else {
+	$f.clarifier_on()
+  }
+});
