dc4b55dc84dc4008183832540a000964f2081d28
Andrea Mangiatordi <andrea.mangiatordi@gmail.com>
2016-05-07 17:46:28 +0200

    Corrected CSS paths, minification is still needed

--- farfalla.js
+++ farfalla.js
@@ -64,8 +64,10 @@ var version = '1.0.0';
   })();
 */
 
-  head.load(farfalla_path+'src/css/farfalla.min.css?v='+version, // this needs a better location
+  head.load(farfalla_path+'src/css/farfalla.css?v='+version, // this needs a better location
+            farfalla_path+'bower_components/jquery-ui/themes/base/jquery-ui.min.css?v='+version,
             farfalla_path+'bower_components/font-awesome/css/font-awesome.min.css?v='+version,
+            farfalla_path+'bower_components/qtip2/jquery.qtip.min.css?v='+version,
             farfalla_path+'backend/pages/lang?v='+version,
 //            farfalla_path+'dist/farfalla.min.js?v='+version);
             farfalla_path+'src/js/vendor/bower.js?v='+version,
